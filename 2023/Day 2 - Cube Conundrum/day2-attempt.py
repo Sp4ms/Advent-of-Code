370fe24260c7b86dabde146203ecbfc7e324045f
Sp4ms <jservaty@FRSecure-L0348.frsecure.local>
2023-12-01 23:55:27 -0700

    Adding Day2 Comments

--- 2023/Day 2 - Cube Conundrum/day2-attempt.py
+++ 2023/Day 2 - Cube Conundrum/day2-attempt.py
@@ -7,9 +7,11 @@ import re, sys
 def conundrum(line):
     bT, gT, rT = 0, 0, 0
     
+    # split line (by each game)
     for n in re.split('; |: |\n', line):
         blue, green, red = 0, 0, 0
 
+        # split each game by color
         for nn in re.split(', ', n):
             if "blue" in re.split('\s', nn):
                 blue = int(re.split('\s', nn)[0])
